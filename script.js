c1f602d46d825368cb0f50e3a262e74448e071c9
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2023-11-01 10:04:54 -0500

    Script tags removed and javascript moved into script.js file

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-let audio = document.getElementById("myAudio");
+window.addEventListener("load", function() {
 
-function playAudio() {
-audio.play();
-}
+//Audio variables
+const durianAudio = document.getElementById("durian");
+const markisaAudio = document.getElementById("markisa");
+const buahNagaAudio = document.getElementById("buah-naga");
+const nangkaAudio = document.getElementById("nangka");
+const rambutanAudio = document.getElementById("rambutan");
+const salakAudio = document.getElementById("salak");
+const belimbingAudio = document.getElementById("belimbing");
+const pepayaAudio = document.getElementById("pepaya");
 
-// function pauseAudio() {
-// audio.pause();
-// }
+//Button variables
+const durianButton = document.getElementById("durianButton");
+const markisaButton = document.getElementById("markisaButton");
+const buahNagaButton = document.getElementById("buahNagaButton");
+const nangkaButton = document.getElementById("nangkaButton");
+const rambutanButton = document.getElementById("rambutanButton");
+const salakButton = document.getElementById("salakButton");
+const belimbingButton = document.getElementById("belimbingButton");
+const pepayaButton = document.getElementById("pepayaButton");
+
+//Audio button functions
+durianButton.addEventListener("click", function () {
+durianAudio.play();
+});
+
+markisaButton.addEventListener("click", function () {
+markisaAudio.play();
+});
+
+buahNagaButton.addEventListener("click", function () {
+buahNagaAudio.play();
+});
+
+nangkaButton.addEventListener("click", function () {
+nangkaAudio.play();
+});
+   
+rambutanButton.addEventListener("click", function () {
+rambutanAudio.play();
+});
+   
+salakButton.addEventListener("click", function (){
+    salakAudio.play();
+});
+   
+belimbingButton.addEventListener("click", function () {
+    belimbingAudio.play();
+});
+
+pepayaButton.addEventListener("click", function () {
+    pepayaAudio.play();
+});
+
+});
